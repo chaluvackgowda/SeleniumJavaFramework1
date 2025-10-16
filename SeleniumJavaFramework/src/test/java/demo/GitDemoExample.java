820710bad90c686458e62f56965bb7faef9d8f8a
abc <abc@example.com>
2025-10-16 09:17:58 +0530

    Created new file GitDemoExample

---
+++ SeleniumJavaFramework/src/test/java/demo/GitDemoExample.java
@@ -0,0 +1,12 @@
+package demo;
+
+public class GitDemoExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Git Demo Example ");
+		
+
+	}
+
+}
